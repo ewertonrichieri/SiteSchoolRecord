71de0a671af8189eb21c91fed42159811121cf09
ewertonrichieri <contato.ewertonrichieri@gmail.com>
2022-11-20 20:18:16 -0300

    CRUD ok

--- js/schoolRecord.js
+++ js/schoolRecord.js
@@ -1,37 +1,50 @@
-const TpEvent = {
-    POST: "POST",
-    PUT: "PUT",
-  };
-
 var SchoolRecord = new (function () {
-    this.getSchoolRecord = getSchoolRecord;
+  this.getSchoolRecord = getSchoolRecord;
 
-    function getSchoolRecord(event) {
-        const schoolRecord = [{
-            Student: {
-                Id: event === TpEvent.POST ? 0 : document.getElementById("txtIdSchRec").value,
-                Name: document.getElementById("txtAluno").value,
-                Email: document.getElementById("txtEmail").value,
-                BirthDate: document.getElementById("txtDtNascimento").value,
-            },
-            Discipline: {
-                Id: TpEvent.POST ? 0 : document.getElementById("txtIdSchRec").value,
-                Name: document.getElementById("txtDisciplina").value,
-                Workload: 80 
-            },
-            Bulletin: {
-                Id: TpEvent.POST ? 0 : document.getElementById("txtIdSchRec").value,
-                IdStudenty: TpEvent.POST ? 0 : document.getElementById("txtIdSchRec").value,
-                DeliveryDate: "2021-11-06T09:00:35"
-            },
-            BulletinGrade: {
-                Id: TpEvent.POST ? 0 : document.getElementById("txtIdSchRec").value,
-                IdDiscipline: TpEvent.POST ? 0 : document.getElementById("txtIdSchRec").value,
-                IdBulletin: TpEvent.POST ? 0 : document.getElementById("txtIdSchRec").value,
-                Grade: 7
-            }}
-    ]
-      return schoolRecord;
-    }
+  function getSchoolRecord() {
+    const schoolRecord = [
+      {
+        Student: {
+          Id: document.getElementById("dvIdStudent").value,
+          Name: document.getElementById("txtAluno").value,
+          Email: document.getElementById("txtEmail").value,
+          BirthDate: document.getElementById("txtDtNascimento").value,
+        },
+        Discipline: {
+          Id: document.getElementById("dvIdDiscipline").value,
+          Name: document.getElementById("txtDisciplina").value,
+          Workload: $("#txtCargaHoraria").val(),
+        },
+        Bulletin: {
+          Id: document.getElementById("dvIdBulletin").value,
+          IdStudenty: document.getElementById("dvIdStudent").value,
+          DeliveryDate: convertToDateTimeIso8601($("#txtDtEntrega").val()),
+        },
+        BulletinGrade: {
+          Id: document.getElementById("dvIdBulletinGrade").value,
+          IdDiscipline: document.getElementById("dvIdDiscipline").value,
+          IdBulletin: document.getElementById("dvIdBulletin").value,
+          Grade: $("#txtNota").val(),
+        },
+      },
+    ];
+    return schoolRecord;
+  }
 })();
 
+function convertToDateTimeIso8601(date) {
+  try {
+    if (!Validation.isNullOrEmpty(date)) {
+      var dateObj = date.split("/");
+      var newDt = `${dateObj[1]}/${dateObj[0]}/${dateObj[2]}`;
+      var dt = new Date(newDt);
+
+      const dtWithTimezone = new Date(dt.valueOf() - dt.getTimezoneOffset() * 60000);
+      return dtWithTimezone.toISOString();
+    } else {
+      return null;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
